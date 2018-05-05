c910ea0c19e46c39c2ed7a991d29da6fb5eaf263
Emma Klint <>
2018-05-05 20:14:53 +0200

    Added router

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <navigation></navigation>
+    <router-view/>
+    <contact></contact>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation'
+import Contact from '@/components/Contact'
+
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+    Contact
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+        {{ msg }}
+        <router-link to="about">About</router-link>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
